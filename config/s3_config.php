052b0175639d12fd8c1f5e72c6d775af5ebcc584
815DINAR <dinarilyasov.helpdeskeddy1@gmail.com>
2025-09-26 14:09:47 +0300

    Add files via upload

--- config/s3_config.php
+++ config/s3_config.php
@@ -5,5 +5,5 @@ define('S3_SECRET_KEY', 'jT1IcBXc2Bv2HKh4d8rK0jttG7h9lxvzzuKbhzai');
 define('S3_ENDPOINT', 's3.regru.cloud');
 define('S3_BUCKET', 'dorama-shorts');
 define('S3_REGION', 'ru-1');
-define('S3_PUBLIC_URL', 'https://dorama-shorts.website.regru.cloud/');
+define('S3_PUBLIC_URL', 'https://s3.regru.cloud/dorama-shorts/');
 ?>
